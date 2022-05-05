dffc281f783ffd5dfd8d4df8b505abe1efdeb487
raskolrcf <101714299+raskolrcf@users.noreply.github.com>
2022-05-05 10:22:14 +0200

    Update streamlit_app.py
    content updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Bueberry Oatmeal')
